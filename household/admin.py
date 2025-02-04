624cff0a977bcb5a5a4e42477bc3852422c66a01
Tom Mart <tom.mart.uk@gmail.com>
2025-02-04 14:21:37 +0000

    change to the inventory app structure

--- household/admin.py
+++ household/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Household, CoOp, HouseholdMember
+from .models import Household, CoOp, HouseholdMember, Inventory, InventoryItem, InventoryItemPurchase
 # Register your models here.
 
 admin.site.register(Household)
 admin.site.register(CoOp)
 admin.site.register(HouseholdMember)
+admin.site.register(Inventory)
+admin.site.register(InventoryItem)
+admin.site.register(InventoryItemPurchase)
--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import Inventory, InventoryItem, InventoryItemPurchase, Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient
 
-admin.site.register(Inventory)
-admin.site.register(InventoryItem)
-admin.site.register(InventoryItemPurchase)
 admin.site.register(Recipe)
 admin.site.register(RecipeIngredient)
